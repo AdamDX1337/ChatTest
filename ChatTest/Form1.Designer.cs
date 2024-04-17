dcccbea2237892ae0dcfe4a3e0ec1edde9a9963d
adamj <adamj@DESKTOP-458JPH2>
2024-04-18 00:01:36 +1000

    Add project files.

---
+++ ChatTest/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace ChatTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(605, 40);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(197, 404);
+            this.listBox1.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(85, 454);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(347, 96);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(438, 470);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 59);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(77, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(34, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(147, 29);
+            this.textBox1.TabIndex = 6;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(187, 40);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 29);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Set";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(347, 28);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(85, 41);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Host";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(438, 28);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(92, 41);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Join";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(714, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(628, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 25);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Online:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(602, 470);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 20);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "No Errors";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.SystemColors.Window;
+            this.richTextBox2.Location = new System.Drawing.Point(85, 89);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(487, 341);
+            this.richTextBox2.TabIndex = 15;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 588);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "ChatTest";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ ChatTest/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+using System.Threading;
+
+namespace ChatTest
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        int onlineCount = 0;
+        string Username = "";
+        Socket clntSock;
+        bool clientOn = false;
+        bool serverOn = false;
+        string nameError = "Invalid Name";
+        string aUser, bUser;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FormClosing += Form_FormClosing;
+            timer1.Start();
+        }
+        public void Form_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                clntSock.Close();
+            }
+        }
+
+            public string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+
+        private void ServerSetup()
+        {
+            serverOn = true;
+            // Constants
+            string host = GetLocalIPAddress(); // Get the local host IP address
+            int port = 42069;
+            
+
+            // Init
+            Socket servSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            servSock.Bind(new IPEndPoint(IPAddress.Parse(host), port));
+            servSock.Listen(1);
+
+            // Wait for client
+            richTextBox2.AppendText("Server started on " + host + ":" + port + "\n");
+            clntSock = servSock.Accept();
+            IPEndPoint clntAddr = (IPEndPoint)clntSock.RemoteEndPoint;
+            richTextBox2.AppendText("Connection started with " + clntAddr.Address + ":" + clntAddr.Port + "\n");
+            string signal = "You have joined the server: " + host + ":" + port + "\n";
+            clntSock.Send(Encoding.UTF8.GetBytes(signal));
+            
+            ChatSetup();
+        }
+
+        public void ClientSetup()
+        {
+            clientOn = true;
+            // Init
+            string hostIP = GetLocalIPAddress(); 
+
+            // Join server
+            clntSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            clntSock.Connect(hostIP, 42069);
+            byte[] buffer = new byte[1024];
+            int bytesRead = clntSock.Receive(buffer);
+            string signal = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            richTextBox2.AppendText(signal);
+            ChatSetup();
+        }
+
+        public void ChatSetup()
+        {
+            // Start a thread to receive messages
+            Thread receiveThread = new Thread(Receive);
+            receiveThread.Start();
+        }
+
+        public void Receive()
+        {
+            while (true)
+            {
+                byte[] buffer = new byte[1024];
+                int bytesRead = clntSock.Receive(buffer);
+                string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                string[] splitMsg = message.Split(':');
+                aUser = splitMsg[0];
+
+                if (!listBox1.Items.Contains(aUser))
+                {
+                    listBox1.Items.Add(aUser);
+                    
+                }
+                if (bUser != aUser)
+                {
+                    listBox1.Items.Remove(bUser);
+                    bUser = aUser;
+                }
+
+                richTextBox2.AppendText(message);
+
+                
+
+
+
+            }
+        }
+
+        //IGNORE
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("a");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            onlineCount = listBox1.Items.Count;
+            label2.Text = onlineCount.ToString();
+        }
+
+        //SET NAME
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Contains(Username))
+            {
+                listBox1.Items.Remove(Username);
+            }
+            Username = textBox1.Text;
+            listBox1.Items.Add(Username);
+        }
+
+        //SEND MESSAGE
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (serverOn || clientOn)
+            {
+                
+                string message = richTextBox1.Text;
+                if (!String.IsNullOrWhiteSpace(message))
+                {
+                    message = Username + ": " + message + "\n";
+                    richTextBox2.AppendText(message);
+                    clntSock.Send(Encoding.UTF8.GetBytes(message));
+                    richTextBox1.Text = "";
+                }
+            }
+        }
+
+        //server
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (clientOn || serverOn)
+            {
+
+            }
+            else
+            {
+                if (String.IsNullOrWhiteSpace(Username))
+                {
+                    label4.Text = nameError;
+                }
+                else
+                {
+                    label4.Text = "No Errors";
+                    if (!clientOn && !serverOn)
+                        ServerSetup();
+                }
+            }
+        }
+
+        //client
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (clientOn || serverOn)
+            {
+
+            }
+            else {
+                if (String.IsNullOrWhiteSpace(Username))
+                {
+                    label4.Text = nameError;
+                }
+                else
+                {
+                    label4.Text = "No Errors";
+                    if (!clientOn && !serverOn)
+                        ClientSetup();
+                }
+            }
+            
+            
+        }
+    }
+}
